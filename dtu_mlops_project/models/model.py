04bfd9656cabd5474bef3b9a8dd61ea13955fed2
berend <b.j.schuit@sron.nl>
2024-01-15 15:17:34 +0100

    pretrained resnet50 model implemntation without TL

---
+++ dtu_mlops_project/models/model.py
@@ -0,0 +1,39 @@
+import torchvision
+from pytorch_lightning import LightningModule
+from torch import optim
+
+class SpeechSpectrogramsTransferLearning(LightningModule):
+    def __init__(self, learning_rate):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.resnet50 = torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V2)
+
+    def forward(self, x):
+
+        x = self.resnet50(x)
+        
+        return x
+        
+
+
+    def training_step(self, batch, batch_idx):
+        data, target = batch
+        preds = self(data)
+        loss = self.criterium(preds, target)
+        acc = (target == preds.argmax(dim=-1)).float().mean()
+        self.log('train_loss', loss)
+        self.log('train_acc', acc)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        data, target = batch
+        preds = self(data)
+        loss = self.criterium(preds, target)
+        acc = (target == preds.argmax(dim=-1)).float().mean()
+        self.log('test_loss', loss)
+        self.log('test_acc', acc)
+        return loss
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.learning_rate) 
+    
